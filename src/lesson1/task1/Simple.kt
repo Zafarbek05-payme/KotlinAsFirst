eee7085fb7934540f6c133615caa7993eeb5de4a
Zafarbek05-payme <zafarbek.j@payme.uz>
2025-03-06 14:49:36 +0500

    all solved

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -131,7 +131,10 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
+fun accountInThreeYears(initial: Double, percent: Double): Double {
+    fun accountInThreeYears(initial: Double, percent: Double): Double {
+    return (initial * (1 + percent/100).pow(3.0))
+}
 
 /**
  * Простая (2 балла)
@@ -139,4 +142,7 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = TODO()
+fun numberRevert(number: Int): Int {
+    var string = number.toString().reversed()
+    return string.toInt()   
+}
